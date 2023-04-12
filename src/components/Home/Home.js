eab5c090f6b045f39e1c5ebeb9ca9f610e47001b
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-04-12 21:34:43 +0200

    add action ADD_LIST

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import Hero from "../Hero/Hero";
+import ListForm from "../ListForm/ListForm";
 import Lists from "../Lists/Lists";
 const Home = () => {
   return (
     <>
       <Hero />
       <Lists />
+      <ListForm />
     </>
   );
 };
